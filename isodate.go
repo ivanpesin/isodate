8d234f82106153fb9fd7b19a36ba6ab219235bc3
Ivan Pesin <ivan.pesin@gmail.com>
2017-08-16 00:38:38 -0400

    Added support for ISO week numbers

--- isodate.go
+++ isodate.go
@@ -12,7 +12,10 @@ import (
 )
 
 var t = time.Now()
-var _, wnr = t.ISOWeek()
+var wyr, wnr = t.UTC().ISOWeek()
+var dow = t.UTC().Weekday()
+
+var tsInfo = make(map[string]string)
 
 var utc = flag.Bool("u", false, "show UTC timestamp")
 var loc = flag.Bool("l", false, "show local timestamp")
@@ -23,25 +26,33 @@ var replace = flag.Bool("r", false, "replace epoch with timestamp")
 var delim = flag.String("d", ",", "show unix epoch")
 var fno = flag.Int("f", 1, "show unix epoch")
 
-func showOneline() {
+func formatTimeStamps() {
+	tsInfo["utc"] = t.UTC().Format("2006-01-02T15:04:05Z")
+	tsInfo["loc"] = t.Format("2006-01-02T15:04:05-0700")
+	tsInfo["epoch"] = fmt.Sprintf("%d", t.Unix())
+	tsInfo["week"] = fmt.Sprintf("%d-W%02d-%d", wyr, wnr, dow)
+	tsInfo["offset"] = t.Format("-0700")
+	tsInfo["tz"] = t.Format("MST")
+}
 
+func showOneline() {
 	buf := bytes.NewBufferString("")
 	switch {
 	case *utc:
-		buf.WriteString(t.UTC().Format("2006-01-02T15:04:05Z"))
+		buf.WriteString(tsInfo["utc"])
 	case *loc:
-		buf.WriteString(t.Format("2006-01-02T15:04:05-0700"))
+		buf.WriteString(tsInfo["loc"])
 	case *epoch:
-		buf.WriteString(fmt.Sprintf("%d", t.Unix()))
+		buf.WriteString(tsInfo["epoch"])
 	case *week:
-		buf.WriteString(fmt.Sprintf("W%02d", wnr))
+		buf.WriteString(tsInfo["week"])
 	default:
-		buf.WriteString(fmt.Sprintf("%s %s %d %s W%02d",
-			t.UTC().Format("2006-01-02T15:04:05Z"),
-			t.Format("2006-01-02T15:04:05-0700"),
-			t.Unix(),
-			t.Format("MST"),
-			wnr))
+		buf.WriteString(fmt.Sprintf("%s %s %s %s %s",
+			tsInfo["utc"],
+			tsInfo["loc"],
+			tsInfo["epoch"],
+			tsInfo["tz"],
+			tsInfo["week"]))
 	}
 	buf.WriteString("\n")
 	fmt.Printf("%s", buf)
@@ -49,12 +60,12 @@ func showOneline() {
 
 func showVerbose() {
 	buf := bytes.NewBufferString("")
-	buf.WriteString(fmt.Sprintf("   UTC: %s\n", t.UTC().Format("2006-01-02T15:04:05Z")))
-	buf.WriteString(fmt.Sprintf(" Local: %s\n", t.Format("2006-01-02T15:04:05-0700")))
-	buf.WriteString(fmt.Sprintf(" Epoch: %d\n", t.Unix()))
-	buf.WriteString(fmt.Sprintf("Offset: %s\n", t.Format("-0700")))
-	buf.WriteString(fmt.Sprintf("    TZ: %s\n", t.Format("MST")))
-	buf.WriteString(fmt.Sprintf("  Week: %d\n", wnr))
+	buf.WriteString(fmt.Sprintf("   UTC: %s\n", tsInfo["utc"]))
+	buf.WriteString(fmt.Sprintf(" Local: %s\n", tsInfo["loc"]))
+	buf.WriteString(fmt.Sprintf(" Epoch: %s\n", tsInfo["epoch"]))
+	buf.WriteString(fmt.Sprintf("Offset: %s\n", tsInfo["offset"]))
+	buf.WriteString(fmt.Sprintf("    TZ: %s\n", tsInfo["tz"]))
+	buf.WriteString(fmt.Sprintf("  Week: %s\n", tsInfo["week"]))
 	fmt.Printf("%s", buf)
 }
 
@@ -70,7 +81,7 @@ func replaceEpoch() {
 		line := s.Text()
 		f := strings.Split(line, *delim)
 		if *fno > len(f) {
-			fmt.Fprintf(os.Stderr, "Error: line %d has only %d fields < %d\n", lno, len(f), *delim)
+			fmt.Fprintf(os.Stderr, "Error: line %d has only %d fields < %d\n", lno, len(f), *fno)
 			continue
 		}
 
@@ -98,11 +109,13 @@ func main() {
 
 	flag.Parse()
 
+	// replace epoch in input
 	if *replace {
 		replaceEpoch()
 		os.Exit(0)
 	}
 
+	// if epoch ts specified on cmd line
 	if len(flag.Args()) > 0 {
 		s, err := strconv.ParseInt(flag.Arg(0), 10, 64)
 		if err != nil {
@@ -110,9 +123,17 @@ func main() {
 			os.Exit(1)
 		}
 		t = time.Unix(s, 0)
-		_, wnr = t.ISOWeek()
+		wyr, wnr = t.UTC().ISOWeek()
+		dow = t.UTC().Weekday()
+	}
+	if dow == 0 {
+		dow = 7
 	}
 
+	// create all timestamps
+	formatTimeStamps()
+
+	// display in requested format
 	if *verbose {
 		showVerbose()
 		os.Exit(0)
